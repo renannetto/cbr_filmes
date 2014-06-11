dd2c2f5efdbcf50dc1fd269fb6194197f6a28405
Renan Oliveira Netto <renan.o.netto@gmail.com>
2014-06-11 19:48:30 -0300

    Script pegando dados do imdb e escrevendo xml com instancias

--- imdb/get_data.py
+++ imdb/get_data.py
@@ -2,9 +2,11 @@ import urllib2
 import json
 
 json_filename = "json"
+instances_file = open("instances", "w")
 
 def get_movie_data(title):
-	html_request = "http://www.omdbapi.com/?i=&t=" + title
+	html_request = "http://www.omdbapi.com/?i=&t=" + title.replace(" ", "+")
+	print "Request: " + html_request
 	response = urllib2.urlopen(html_request)
 	json_text = response.read()
 
@@ -16,10 +18,27 @@ def get_movie_data(title):
 	movie_data = json.load(json_file) 
 	json_file.close()
 
-	print "Title: " + movie_data["Title"]
-	print "Genre: " + movie_data["Genre"]
-	print "Year: " + movie_data["Year"]
-	print "Language: " + movie_data["Language"]
-	print "Rating: " + movie_data["imdbRating"]
+	title = movie_data["Title"]
+	year = movie_data["Year"]
+	language = movie_data["Language"]
+	language = language.replace(",", ";") + ";"
+	rating = movie_data["imdbRating"]
+	genre = movie_data["Genre"]
+	genre = genre.replace(", ", ";") + ";"
 
-get_movie_data("Frozen")
+	instances_file.write('\t<instance id="' + title + '" >\n')
+	instances_file.write('\t\t<att name="Titulo" value="' + title + '" />\n')	
+	instances_file.write('\t\t<att name="Ano" value="' + year + '" />\n')	
+	instances_file.write('\t\t<att name="Idioma" value="' + language + '" />\n')	
+	instances_file.write('\t\t<att name="Nota" value="' + rating + '" />\n')	
+	instances_file.write('\t\t<att name="Genero" value="' + genre + '" />\n')	
+	instances_file.write("\t</instance>\n")
+
+
+movies_file = open("movie_list", "r")
+movies = movies_file.read()
+movie_list = movies.split("\n")
+
+for movie in movie_list:
+	get_movie_data(movie)
+
